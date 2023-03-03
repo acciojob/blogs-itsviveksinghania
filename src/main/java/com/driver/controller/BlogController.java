e425db11a5b216a8838d6abfa223bb5673efd29d
itsviveksinghania <vickykumarsify@gmail.com>
2023-03-03 14:54:21 +0530

    Solution

--- src/main/java/com/driver/controller/BlogController.java
+++ src/main/java/com/driver/controller/BlogController.java
@@ -13,18 +13,35 @@ import java.util.List;
 @RequestMapping("/blogs")
 public class BlogController {
 
+    @Autowired
+    BlogService blogService;
+
     @PostMapping
-    public ResponseEntity createBlog(@RequestParam Integer userId ,
+    public ResponseEntity<Void> createBlog(@RequestParam Integer userId ,
                                      @RequestParam String title,
                                      @RequestParam String content) {
         // Create a blog and add it under given user
+        blogService.createAndReturnBlog(userId, title, content);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{blogId}")
     public ResponseEntity<Void> deleteBlog(@PathVariable int blogId) {
         // Delete the blog using deleteById
+        blogService.deleteBlog(blogId);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<Integer> getAllBlogs() {
+        List<Blog> blogs = blogService.showBlogs();
+        return new ResponseEntity<>(blogs.size(), HttpStatus.OK);
+    }
+
+    @PutMapping("/{blogId}/add-image")
+    public ResponseEntity<String> addImage(@PathVariable int blogId, @RequestParam String description, @RequestParam String dimensions) {
+        blogService.addImage(blogId, description, dimensions);
+        return new ResponseEntity<>("Added image successfully", HttpStatus.OK);
     }
 }
 
